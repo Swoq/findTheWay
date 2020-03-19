ebcd309b755a362365917578b22ace47df6424d3
Mykhaylo Kratyuk <misha.kratyuk@gmail.com>
2020-03-19 16:44:10 +0200

    Implementation of the Dijkstra algorithm

--- src/Dijkstra.java
+++ src/Dijkstra.java
@@ -1,5 +1,50 @@
-public class Dijkstra {
+import java.util.Arrays;
 
+import static java.util.Arrays.fill;
+
+public class Dijkstra {
+    int INF = Integer.MAX_VALUE / 2; 
+    int vNum;
+    MultiList graph;
+
+    void dijkstraRMQ(int start, int end) {
+        boolean[] used = new boolean[vNum];
+        int[] prev = new int[vNum];
+        int[] dist = new int[vNum];
+        RMQ rmq = new RMQ(vNum);
+
+        fill(prev, -1);
+        fill(dist, INF);
+
+        rmq.set(start, dist[start] = 0);
+
+        for (; ;) {
+            int v = rmq.minIndex();
+            if (v == -1 || v == end) break;
+
+            used[v] = true;
+
+            for (int i = graph.head[v]; i != 0; i = graph.next[i]) {
+                int nv = graph.vert[i];
+                int cost = graph.cost[i];
+                if (!used[nv] && dist[nv] > dist[v] + cost) {
+                    rmq.set(nv, dist[nv] = dist[v] + cost);
+                    prev[nv] = v;
+                }
+            }
+        }
+
+        Stack stack = new Stack();
+        for (int v = end; v != -1; v = prev[v]) {
+            stack.add(v);
+        }
+        int[] sp = new int[stack.size()];
+        for (int i = 0; i < sp.length; i++)
+            sp[i] = stack.poll() + 1;
+
+        System.out.printf("Кратчайшее расстояние между %d и %d = %d%n", start + 1, end + 1, dist[end]);
+        System.out.println("Кратчайший путь: " + Arrays.toString(sp));
+    }
 
     static class MultiList {
         int[] head;
@@ -10,9 +55,9 @@ public class Dijkstra {
 
         MultiList(int vNum, int eNum) {
             head = new int[vNum];
-            next = new int[eNum * 2 + 1];
-            vert = new int[eNum * 2 + 1];
-            cost = new int[eNum * 2 + 1];
+            next = new int[eNum*2 + 1];
+            vert = new int[eNum*2 + 1];
+            cost = new int[eNum*2 + 1];
         }
 
         void add(int u, int v, int w) {
